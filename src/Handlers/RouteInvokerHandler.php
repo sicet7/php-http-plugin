95d1635a230d88da0deb68b33740c30410a3f730
Martin René Sørensen <git@sicet7.com>
2023-04-15 23:06:02 +0200

    FIX: missing import of correct HttpException.

--- src/Handlers/RouteInvokerHandler.php
+++ src/Handlers/RouteInvokerHandler.php
@@ -5,10 +5,10 @@ namespace Sicet7\HTTP\Handlers;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Sicet7\HTTP\Exceptions\HttpException;
 use Sicet7\HTTP\Interfaces\HandlerContainerInterface;
 use Sicet7\HTTP\RequestAttributes\PathArguments;
 use Sicet7\HTTP\RequestAttributes\RoutingResult;
+use Sicet7\HttpUtils\Exceptions\HttpException;
 
 readonly class RouteInvokerHandler implements RequestHandlerInterface
 {
